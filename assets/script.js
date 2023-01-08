b424acb3ea17e8334d2a9c0100da615d7fc9a537
HIQMALISM <113483636+HER-hiq@users.noreply.github.com>
2023-01-08 20:29:01 +0700

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,83 @@
+const team1Score = document.querySelector(".timl_poin .poin_l");
+const team2Score = document.querySelector(".timr_poin .poin_r");
+
+team1Score.addEventListener("pointerdown", handlePointerDown);
+team1Score.addEventListener("pointerup", handlePointerUp);
+
+team2Score.addEventListener("pointerdown", handlePointerDown);
+team2Score.addEventListener("pointerup", handlePointerUp);
+
+let initialY = null;
+
+function handlePointerDown(event) {
+  initialY = event.clientY;
+}
+
+function handlePointerUp(event) {
+  if (initialY === null) {
+    return;
+  }
+
+  const currentY = event.clientY;
+  const diffY = initialY - currentY;
+
+  if (diffY > 0) {
+    if (event.target.classList.contains("poin_l")) {
+      team1Score.textContent = parseInt(team1Score.textContent) +1;
+    } else if (event.target.classList.contains("poin_r")) {
+      team2Score.textContent = parseInt(team2Score.textContent) +1;
+    }
+  } else {
+    if (event.target.classList.contains("poin_l")) {
+      if (team1Score.textContent > 0) {
+        team1Score.textContent = parseInt(team1Score.textContent) -1;
+      }
+    } else if (event.target.classList.contains("poin_r")) {
+      if (team2Score.textContent > 0) {
+        team2Score.textContent = parseInt(team2Score.textContent) -1;
+      }
+    }
+  }
+
+  initialY = null;
+}
+
+
+team1Score.addEventListener("touchstart", handleTouchStart);
+team1Score.addEventListener("touchend", handleTouchEnd);
+
+team2Score.addEventListener("touchstart", handleTouchStart);
+team2Score.addEventListener("touchend", handleTouchEnd);
+
+function handleTouchStart(event) {
+  initialY = event.touches[0].clientY;
+}
+
+function handleTouchEnd(event) {
+  if (initialY === null) {
+    return;
+  }
+
+  const currentY = event.changedTouches[0].clientY;
+  const diffY = initialY - currentY;
+
+  if (diffY > 0) {
+    if (event.target.classList.contains("poin_l")) {
+      team1Score.textContent = parseInt(team1Score.textContent) +1;
+    } else if (event.target.classList.contains("poin_r")) {
+      team2Score.textContent = parseInt(team2Score.textContent) +1;
+    }
+  } else {
+    if (event.target.classList.contains("poin_l")) {
+      if (team1Score.textContent > 0) {
+        team1Score.textContent = parseInt(team1Score.textContent) -1;
+      }
+    } else if (event.target.classList.contains("poin_r")) {
+      if (team2Score.textContent > 0) {
+        team2Score.textContent = parseInt(team2Score.textContent) -1;
+      }
+    }
+  }
+
+  initialY = null;
+}
